1f0765faac93084f4acf18cd723d1694984f5708
msucil <np.msushil@gmail.com>
2018-04-20 16:16:29 +0545

    Fix wrong import Text

--- hadoop/src/main/java/com/msucil/hadoop/wordcount/WordCountReducer.java
+++ hadoop/src/main/java/com/msucil/hadoop/wordcount/WordCountReducer.java
@@ -1,9 +1,9 @@
 package com.msucil.hadoop.wordcount;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import javax.xml.soap.Text;
 import java.io.IOException;
 
 /**
